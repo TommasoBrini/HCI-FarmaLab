c67e97faf8f1a02a7121fe61ea808f12e4dc3b1d
Tommaso Brini <tommaso.brini@studio.unibo.it>
2025-12-20 16:39:50 +0100

    chore(frontend): inizialize vue app

---
+++ frontend/farmalab/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg">
+      <h1 class="text-3xl font-bold text-blue-600">
+        Il Mio Progetto Vue
+      </h1>
+      <p class="mt-4 text-gray-600">
+        Let's Start
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Il tuo codice JavaScript/TypeScript qui
+</script>
